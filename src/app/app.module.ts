e0e702c9f23f5ca4b8c2dd56dc78f91878feb9be
face123@ <shinikurian009@gmail.com>
2020-04-30 19:24:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ServicesComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
